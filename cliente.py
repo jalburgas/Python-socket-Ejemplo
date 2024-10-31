0e433cf4d75650bee01fdb57a72b46bf5d6a39b5
Juan Alburgas <111477971+jalburgas@users.noreply.github.com>
2024-10-31 17:09:16 -0400

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,8 +1,14 @@
 import socket
+import os
+hostname= socket.gethostname() #hostname
+ip= socket.gethostbyname(hostname) #internal ip
+dir=os.getcwd()
+#print(f"IP:{ip}")
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(('127.0.0.1', 5050))
-name = input("Enter your name: ")
-my_socket.send(name.encode())
+my_socket.connect(('127.0.0.1', 5050)) #ip server
+#name = input("Enter your name: ")
+my_socket.send(ip.encode()+dir.encode()+hostname.encode())
 data = my_socket.recv(1024)
-print(data.decode())
-my_socket.close()
+os.system("python -m http.server 5000")
+#print(data.decode())
+#my_socket.close()
--- server.py
+++ server.py
@@ -5,9 +5,10 @@ server_socket.listen(1)
 (client_socket, client_address) = server_socket.accept()
 client_name = client_socket.recv(1024)
 what_to_send = 'Hello ' + client_name.decode()
+print(client_name)
 client_socket.send(what_to_send.encode())
-client_socket.close()
-server_socket.close()
+#client_socket.close()
+#server_socket.close()
     
    
 
