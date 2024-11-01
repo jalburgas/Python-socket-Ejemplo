9577cd9dfd825950e6ca94c5bf9daf542f65409d
Juan Alburgas <111477971+jalburgas@users.noreply.github.com>
2024-10-31 23:39:52 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 5050))
+server_socket.bind(('0.0.0.0', 5000))
 server_socket.listen(1)
 (client_socket, client_address) = server_socket.accept()
 client_name = client_socket.recv(1024)
